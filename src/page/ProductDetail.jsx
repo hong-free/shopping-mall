57570ec5de378592dd2db76534093a98be79a50a
Honsungyeon <hosuye@gamil.com>
2025-10-30 01:23:31 +0900

    쇼핑몰이4

--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -7,7 +7,8 @@ const ProductDetail = () => {
   let { id } = useParams();
   const [product, setProduct] = useState(null);
   const getProductDetail = async () => {
-    let url = `https://my-json-server.typicode.com/hong-free/shopping-mal/products/${id}`;
+    let url = `http://localhost:5000/products/s/${id}`;
+    // let url = `https://my-json-server.typicode.com/hong-free/shopping-mal/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     console.log(data);
